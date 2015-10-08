db100cde65f39cdb3988788238485787f6c9e2f6
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-10-08 15:30:23 -0500

    add blockdev mounting testcase
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- test/main.sh
+++ test/main.sh
@@ -203,6 +203,11 @@ wipe() {
     kill -9 ${pid}
   done
 
+  if [ -f ${TEST_DIR}/loops ]; then
+    cat ${TEST_DIR}/loops  | while read line; do
+      losetup -d $line || true
+    done
+  fi
   if mountpoint -q "${1}"; then
     umount "${1}"
   fi
